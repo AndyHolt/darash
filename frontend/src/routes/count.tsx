06168f62e7bc4b3e38c92b13f9afb6bdccbd817a
Andy Holt <andrew.holt@hotmail.co.uk>
2026-04-21 23:40:51 +0100

    configure tanstack router and query

---
+++ frontend/src/routes/count.tsx
@@ -0,0 +1,27 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query'
+
+interface WordCount {
+    count: number,
+}
+
+const countQueryOptions = queryOptions({
+    queryKey: ['count'],
+    queryFn: ({ signal }) => fetch(`/api/count`, { signal })
+        .then(res => {
+        if (!res.ok) throw new Error(`HTTP ${res.status}`)
+        return res.json() as Promise<WordCount>
+    })
+})
+
+export const Route = createFileRoute('/count')({
+    loader: ({ context: { queryClient }}) => queryClient.ensureQueryData(countQueryOptions),
+    component: () => {
+        const { data: { count } } = useSuspenseQuery(countQueryOptions)
+
+        return (
+            <div>Word count: {count}</div>
+        )
+
+    }
+})
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        // Ensure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true,
+        }),
+        react()
+    ],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+        },
+    },
 })
