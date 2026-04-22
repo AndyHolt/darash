6b6e04985690279bd49a2f1805ff02ebf291cbdd
Andy Holt <andrew.holt@hotmail.co.uk>
2026-04-22 16:19:02 +0100

    replace header links with link styled buttons

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,19 +1,26 @@
 import type { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Button } from "@/components/ui/button";
 
 const RootLayout = () => (
   <>
     <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{" "}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>{" "}
-      <Link to="/count" className="[&.active]:font-bold">
-        Count
-      </Link>
+      <Button variant="link" asChild>
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>
+      </Button>
+      <Button variant="link" asChild>
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </Button>
+      <Button variant="link" asChild>
+        <Link to="/count" className="[&.active]:font-bold">
+          Count
+        </Link>
+      </Button>
     </div>
     <hr />
     <Outlet />
