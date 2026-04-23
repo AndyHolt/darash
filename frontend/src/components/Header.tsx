02365dee039f096a1f682f1a274ab26a3ea56002
Andy Holt <andrew.holt@hotmail.co.uk>
2026-04-23 02:12:27 +0100

    Header bar styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,19 @@
+import { Link } from "@tanstack/react-router";
 import { ModeToggle } from "@/components/color-theme/ModeToggle";
 import LinkButton from "@/components/LinkButton";
 
 export default function Header() {
   return (
     <>
-      <div className="p-2 flex gap-2">
-        <LinkButton to="/">Home</LinkButton>
-        <LinkButton to="/about">About</LinkButton>
-        <LinkButton to="/count">Count</LinkButton>
-        <ModeToggle />
+      <div className="p-2 flex justify-between items-center bg-sidebar">
+        <Link to="/">
+          <h1 className="font-bold text-2xl text-sidebar-foreground select-none">Darash</h1>
+        </Link>
+        <div className="flex gap-1">
+          <LinkButton to="/about">About</LinkButton>
+          <LinkButton to="/count">Count</LinkButton>
+          <ModeToggle />
+        </div>
       </div>
       <hr />
     </>
