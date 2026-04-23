67bbd1ce37e54d0ba88d17f65218eb8e201b60ad
Andy Holt <andrew.holt@hotmail.co.uk>
2026-04-23 12:24:27 +0100

    add icon to header bar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,8 +6,9 @@ export default function Header() {
   return (
     <>
       <div className="p-2 flex justify-between items-center bg-sidebar">
-        <Link to="/">
-          <h1 className="font-bold text-xl text-sidebar-foreground select-none">Darash</h1>
+        <Link to="/" className="flex items-center gap-1.5">
+          <img src="/icon-header.svg" alt="" className="h-6 w-6" />
+          <h1 className="font-medium text-xl text-primary select-none">Darash</h1>
         </Link>
         <div className="flex gap-1">
           <LinkButton to="/about">About</LinkButton>
