87592a056cca84083a43284ebbd97555edd0cdf9
Andy Holt <andrew.holt@hotmail.co.uk>
2026-04-23 01:42:47 +0100

    light/dark mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@/components/color-theme/ThemeProvider";
 
 // Import generated route tree
 import { routeTree } from "./routeTree.gen";
@@ -27,7 +28,9 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <RouterProvider router={router} />
+        </ThemeProvider>
       </QueryClientProvider>
     </StrictMode>,
   );
