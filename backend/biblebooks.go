feaae416f392997832395cdb7050e0040af2b966
Andy Holt <andrew.holt@hotmail.co.uk>
2026-04-24 17:21:46 +0100

    update book names and abrevs to SBL conventions

--- backend/biblebooks.go
+++ backend/biblebooks.go
@@ -105,31 +105,31 @@ func (b Book) VersesInChapter(chapter int) (int, error) {
 // Verse numbers per chapter from: https://www.life-everlasting.net/pages/bible/Number%20of%20verses%20per%20chapter%20in%20Bible%20(KJV).php
 var books = map[BookID]Book{
 	Genesis: {
-		ID: Genesis, Name: "Genesis", Abbrev: "Genesis", Testament: OldTestament,
+		ID: Genesis, Name: "Genesis", Abbrev: "Gen", Testament: OldTestament,
 		Verses: []int{31, 25, 24, 26, 32, 22, 24, 22, 29, 32, 32, 20, 18, 24, 21, 16, 27, 33, 38, 18, 34, 24, 20, 67, 34, 35, 46, 22, 35, 43, 54, 33, 20, 31, 29, 43, 36, 30, 23, 23, 57, 38, 34, 34, 28, 34, 31, 22, 33, 26},
 	},
 	Exodus: {
-		ID: Exodus, Name: "Exodus", Abbrev: "Exodus", Testament: OldTestament,
+		ID: Exodus, Name: "Exodus", Abbrev: "Exod", Testament: OldTestament,
 		Verses: []int{22, 25, 22, 31, 23, 30, 29, 28, 35, 29, 10, 51, 22, 31, 27, 36, 16, 27, 25, 26, 37, 30, 33, 18, 40, 37, 21, 43, 46, 38, 18, 35, 23, 35, 35, 38, 29, 31, 43, 38},
 	},
 	Leviticus: {
-		ID: Leviticus, Name: "Leviticus", Abbrev: "Leviticus", Testament: OldTestament,
+		ID: Leviticus, Name: "Leviticus", Abbrev: "Lev", Testament: OldTestament,
 		Verses: []int{17, 16, 17, 35, 26, 23, 38, 36, 24, 20, 47, 8, 59, 57, 33, 34, 16, 30, 37, 27, 24, 33, 44, 23, 55, 46, 34},
 	},
 	Numbers: {
-		ID: Numbers, Name: "Numbers", Abbrev: "Numbers", Testament: OldTestament,
+		ID: Numbers, Name: "Numbers", Abbrev: "Num", Testament: OldTestament,
 		Verses: []int{54, 34, 51, 49, 31, 27, 89, 26, 23, 36, 35, 16, 33, 45, 41, 35, 28, 32, 22, 29, 35, 41, 30, 25, 18, 65, 23, 31, 39, 17, 54, 42, 56, 29, 34, 13},
 	},
 	Deuteronomy: {
-		ID: Deuteronomy, Name: "Deuteronomy", Abbrev: "Deuteronomy", Testament: OldTestament,
+		ID: Deuteronomy, Name: "Deuteronomy", Abbrev: "Deut", Testament: OldTestament,
 		Verses: []int{46, 37, 29, 49, 33, 25, 26, 20, 29, 22, 32, 31, 19, 29, 23, 22, 20, 22, 21, 20, 23, 29, 26, 22, 19, 19, 26, 68, 29, 20, 30, 52, 29, 12},
 	},
 	Joshua: {
-		ID: Joshua, Name: "Joshua", Abbrev: "Joshua", Testament: OldTestament,
+		ID: Joshua, Name: "Joshua", Abbrev: "Josh", Testament: OldTestament,
 		Verses: []int{18, 24, 17, 24, 15, 27, 26, 35, 27, 43, 23, 24, 33, 15, 63, 10, 18, 28, 51, 9, 45, 34, 16, 33},
 	},
 	Judges: {
-		ID: Judges, Name: "Judges", Abbrev: "Judges", Testament: OldTestament,
+		ID: Judges, Name: "Judges", Abbrev: "Judg", Testament: OldTestament,
 		Verses: []int{36, 23, 31, 24, 31, 40, 25, 35, 57, 18, 40, 15, 25, 20, 20, 31, 13, 31, 30, 48, 25},
 	},
 	Ruth: {
@@ -137,27 +137,27 @@ var books = map[BookID]Book{
 		Verses: []int{22, 23, 18, 22},
 	},
 	FirstSamuel: {
-		ID: FirstSamuel, Name: "FirstSamuel", Abbrev: "FirstSamuel", Testament: OldTestament,
+		ID: FirstSamuel, Name: "1 Samuel", Abbrev: "1Sam", Testament: OldTestament,
 		Verses: []int{28, 36, 21, 22, 12, 21, 17, 22, 27, 27, 15, 25, 23, 52, 35, 23, 58, 30, 24, 42, 16, 23, 28, 23, 43, 25, 12, 25, 11, 31, 13},
 	},
 	SecondSamuel: {
-		ID: SecondSamuel, Name: "SecondSamuel", Abbrev: "SecondSamuel", Testament: OldTestament,
+		ID: SecondSamuel, Name: "2 Samuel", Abbrev: "2Sam", Testament: OldTestament,
 		Verses: []int{27, 32, 39, 12, 25, 23, 29, 18, 13, 19, 27, 31, 39, 33, 37, 23, 29, 32, 44, 26, 22, 51, 39, 25},
 	},
 	FirstKings: {
-		ID: FirstKings, Name: "FirstKings", Abbrev: "FirstKings", Testament: OldTestament,
+		ID: FirstKings, Name: "1 Kings", Abbrev: "1Kgs", Testament: OldTestament,
 		Verses: []int{53, 46, 28, 34, 18, 38, 51, 66, 28, 29, 43, 33, 34, 31, 34, 34, 24, 46, 21, 43, 29, 53},
 	},
 	SecondKings: {
-		ID: SecondKings, Name: "SecondKings", Abbrev: "SecondKings", Testament: OldTestament,
+		ID: SecondKings, Name: "2 Kings", Abbrev: "2Kgs", Testament: OldTestament,
 		Verses: []int{18, 25, 27, 44, 27, 33, 20, 29, 37, 36, 20, 22, 25, 29, 38, 20, 41, 37, 37, 21, 26, 20, 37, 20, 30},
 	},
 	FirstChronicles: {
-		ID: FirstChronicles, Name: "FirstChronicles", Abbrev: "FirstChronicles", Testament: OldTestament,
+		ID: FirstChronicles, Name: "1 Chronicles", Abbrev: "1Chr", Testament: OldTestament,
 		Verses: []int{54, 55, 24, 43, 26, 81, 40, 40, 44, 14, 47, 40, 14, 17, 29, 43, 27, 17, 19, 8, 30, 19, 32, 31, 31, 32, 34, 21, 30},
 	},
 	SecondChronicles: {
-		ID: SecondChronicles, Name: "SecondChronicles", Abbrev: "SecondChronicles", Testament: OldTestament,
+		ID: SecondChronicles, Name: "2 Chronicles", Abbrev: "2Chr", Testament: OldTestament,
 		Verses: []int{17, 18, 17, 22, 14, 42, 22, 18, 31, 19, 23, 16, 22, 15, 19, 14, 19, 34, 11, 37, 20, 12, 21, 27, 28, 23, 9, 27, 36, 27, 21, 33, 25, 33, 27, 23},
 	},
 	Ezra: {
@@ -165,11 +165,11 @@ var books = map[BookID]Book{
 		Verses: []int{11, 70, 13, 24, 17, 22, 28, 36, 15, 44},
 	},
 	Nehemiah: {
-		ID: Nehemiah, Name: "Nehemiah", Abbrev: "Nehemiah", Testament: OldTestament,
+		ID: Nehemiah, Name: "Nehemiah", Abbrev: "Neh", Testament: OldTestament,
 		Verses: []int{11, 20, 32, 23, 19, 19, 73, 18, 38, 39, 36, 47, 31},
 	},
 	Esther: {
-		ID: Esther, Name: "Esther", Abbrev: "Esther", Testament: OldTestament,
+		ID: Esther, Name: "Esther", Abbrev: "Esth", Testament: OldTestament,
 		Verses: []int{22, 23, 15, 17, 14, 14, 10, 17, 32, 3},
 	},
 	Job: {
@@ -177,45 +177,45 @@ var books = map[BookID]Book{
 		Verses: []int{22, 13, 26, 21, 27, 30, 21, 22, 35, 22, 20, 25, 28, 22, 35, 22, 16, 21, 29, 29, 34, 30, 17, 25, 6, 14, 23, 28, 25, 31, 40, 22, 33, 37, 16, 33, 24, 41, 30, 24, 34, 17},
 	},
 	Psalms: {
-		ID: Psalms, Name: "Psalms", Abbrev: "Psalms", Testament: OldTestament,
+		ID: Psalms, Name: "Psalms", Abbrev: "Ps", Testament: OldTestament,
 		Verses: []int{
 			6, 12, 8, 8, 12, 10, 17, 9, 20, 18, 7, 8, 6, 7, 5, 11, 15, 50, 14, 9, 13, 31, 6, 10, 22, 12, 14, 9, 11, 12, 24, 11, 22, 22, 28, 12, 40, 22, 13, 17, 13, 11, 5, 26, 17, 11, 9, 14, 20, 23, 19, 9, 6, 7, 23, 13, 11, 11, 17, 12, 8, 12, 11, 10, 13, 20, 7, 35, 36, 5, 24, 20, 28, 23, 10, 12, 20, 72, 13, 19, 16, 8, 18, 12, 13, 17, 7, 18, 52, 17, 16, 15, 5, 23, 11, 13, 12, 9, 9, 5, 8, 28, 22, 35, 45, 48, 43, 13, 31, 7, 10, 10, 9, 8, 18, 19, 2, 29, 176, 7, 8, 9, 4, 8, 5, 6, 5, 6, 8, 8, 3, 18, 3, 3, 21, 26, 9, 8, 24, 13, 10, 7, 12, 15, 21, 10, 20, 14, 9, 6,
 		},
 	},
 	Proverbs: {
-		ID: Proverbs, Name: "Proverbs", Abbrev: "Proverbs", Testament: OldTestament,
+		ID: Proverbs, Name: "Proverbs", Abbrev: "Prov", Testament: OldTestament,
 		Verses: []int{33, 22, 35, 27, 23, 35, 27, 36, 18, 32, 31, 28, 25, 35, 33, 33, 28, 24, 29, 30, 31, 29, 35, 34, 28, 28, 27, 28, 27, 33, 31},
 	},
 	Ecclesiastes: {
-		ID: Ecclesiastes, Name: "Ecclesiastes", Abbrev: "Ecclesiastes", Testament: OldTestament,
+		ID: Ecclesiastes, Name: "Ecclesiastes", Abbrev: "Eccl", Testament: OldTestament,
 		Verses: []int{18, 26, 22, 17, 19, 12, 29, 17, 18, 20, 10, 14},
 	},
 	SongOfSolomon: {
-		ID: SongOfSolomon, Name: "SongOfSolomon", Abbrev: "SongOfSolomon", Testament: OldTestament,
+		ID: SongOfSolomon, Name: "Song of Songs", Abbrev: "Song", Testament: OldTestament,
 		Verses: []int{17, 17, 11, 16, 16, 12, 14, 14},
 	},
 	Isaiah: {
-		ID: Isaiah, Name: "Isaiah", Abbrev: "Isaiah", Testament: OldTestament,
+		ID: Isaiah, Name: "Isaiah", Abbrev: "Isa", Testament: OldTestament,
 		Verses: []int{31, 22, 26, 6, 30, 13, 25, 22, 21, 34, 16, 6, 22, 32, 9, 14, 14, 7, 25, 6, 17, 25, 18, 23, 12, 21, 13, 29, 24, 33, 9, 20, 24, 17, 10, 22, 38, 22, 8, 31, 29, 25, 28, 28, 25, 13, 15, 22, 26, 11, 23, 15, 12, 17, 13, 12, 21, 14, 21, 22, 11, 12, 19, 12, 25, 24},
 	},
 	Jeremiah: {
-		ID: Jeremiah, Name: "Jeremiah", Abbrev: "Jeremiah", Testament: OldTestament,
+		ID: Jeremiah, Name: "Jeremiah", Abbrev: "Jer", Testament: OldTestament,
 		Verses: []int{19, 37, 25, 31, 31, 30, 34, 23, 25, 25, 23, 17, 27, 22, 21, 21, 27, 23, 15, 18, 14, 30, 40, 10, 38, 24, 22, 17, 32, 24, 40, 44, 26, 22, 19, 32, 21, 28, 18, 16, 18, 22, 13, 30, 5, 28, 7, 47, 39, 46, 64, 34},
 	},
 	Lamentations: {
-		ID: Lamentations, Name: "Lamentations", Abbrev: "Lamentations", Testament: OldTestament,
+		ID: Lamentations, Name: "Lamentations", Abbrev: "Lam", Testament: OldTestament,
 		Verses: []int{22, 22, 66, 22, 22},
 	},
 	Ezekiel: {
-		ID: Ezekiel, Name: "Ezekiel", Abbrev: "Ezekiel", Testament: OldTestament,
+		ID: Ezekiel, Name: "Ezekiel", Abbrev: "Ezek", Testament: OldTestament,
 		Verses: []int{28, 10, 27, 17, 17, 14, 27, 18, 11, 22, 25, 28, 23, 23, 8, 63, 24, 32, 14, 44, 37, 31, 49, 27, 17, 21, 36, 26, 21, 26, 18, 32, 33, 31, 15, 38, 28, 23, 29, 49, 26, 20, 27, 31, 25, 24, 23, 35},
 	},
 	Daniel: {
-		ID: Daniel, Name: "Daniel", Abbrev: "Daniel", Testament: OldTestament,
+		ID: Daniel, Name: "Daniel", Abbrev: "Dan", Testament: OldTestament,
 		Verses: []int{21, 49, 30, 37, 31, 28, 28, 27, 27, 21, 45, 13},
 	},
 	Hosea: {
-		ID: Hosea, Name: "Hosea", Abbrev: "Hosea", Testament: OldTestament,
+		ID: Hosea, Name: "Hosea", Abbrev: "Hos", Testament: OldTestament,
 		Verses: []int{9, 25, 5, 19, 15, 11, 16, 14, 17, 15, 11, 15, 15, 10},
 	},
 	Joel: {
@@ -227,7 +227,7 @@ var books = map[BookID]Book{
 		Verses: []int{15, 16, 15, 13, 27, 14, 17, 14, 15},
 	},
 	Obadiah: {
-		ID: Obadiah, Name: "Obadiah", Abbrev: "Obadiah", Testament: OldTestament,
+		ID: Obadiah, Name: "Obadiah", Abbrev: "Obad", Testament: OldTestament,
 		Verses: []int{21},
 	},
 	Jonah: {
@@ -235,35 +235,35 @@ var books = map[BookID]Book{
 		Verses: []int{16, 11, 10, 11},
 	},
 	Micah: {
-		ID: Micah, Name: "Micah", Abbrev: "Micah", Testament: OldTestament,
+		ID: Micah, Name: "Micah", Abbrev: "Mic", Testament: OldTestament,
 		Verses: []int{16, 13, 12, 14, 14, 16, 20},
 	},
 	Nahum: {
-		ID: Nahum, Name: "Nahum", Abbrev: "Nahum", Testament: OldTestament,
+		ID: Nahum, Name: "Nahum", Abbrev: "Nah", Testament: OldTestament,
 		Verses: []int{14, 14, 19},
 	},
 	Habakkuk: {
-		ID: Habakkuk, Name: "Habakkuk", Abbrev: "Habakkuk", Testament: OldTestament,
+		ID: Habakkuk, Name: "Habakkuk", Abbrev: "Hab", Testament: OldTestament,
 		Verses: []int{17, 20, 19},
 	},
 	Zephaniah: {
-		ID: Zephaniah, Name: "Zephaniah", Abbrev: "Zephaniah", Testament: OldTestament,
+		ID: Zephaniah, Name: "Zephaniah", Abbrev: "Zeph", Testament: OldTestament,
 		Verses: []int{18, 15, 20},
 	},
 	Haggai: {
-		ID: Haggai, Name: "Haggai", Abbrev: "Haggai", Testament: OldTestament,
+		ID: Haggai, Name: "Haggai", Abbrev: "Hag", Testament: OldTestament,
 		Verses: []int{15, 23},
 	},
 	Zechariah: {
-		ID: Zechariah, Name: "Zechariah", Abbrev: "Zechariah", Testament: OldTestament,
+		ID: Zechariah, Name: "Zechariah", Abbrev: "Zech", Testament: OldTestament,
 		Verses: []int{17, 17, 10, 14, 11, 15, 14, 23, 17, 12, 17, 14, 9, 21},
 	},
 	Malachi: {
-		ID: Malachi, Name: "Malachi", Abbrev: "Malachi", Testament: OldTestament,
+		ID: Malachi, Name: "Malachi", Abbrev: "Mal", Testament: OldTestament,
 		Verses: []int{14, 17, 24},
 	},
 	Matthew: {
-		ID: Matthew, Name: "Matthew", Abbrev: "Matthew", Testament: NewTestament,
+		ID: Matthew, Name: "Matthew", Abbrev: "Matt", Testament: NewTestament,
 		Verses: []int{25, 23, 17, 25, 48, 34, 29, 34, 38, 42, 30, 50, 58, 36, 39, 28, 27, 35, 30, 34, 46, 46, 39, 51, 46, 75, 66, 20},
 	},
 	Mark: {
@@ -283,47 +283,47 @@ var books = map[BookID]Book{
 		Verses: []int{26, 47, 26, 37, 42, 15, 60, 40, 43, 48, 30, 25, 52, 28, 41, 40, 34, 28, 41, 38, 40, 30, 35, 27, 27, 32, 44, 31},
 	},
 	Romans: {
-		ID: Romans, Name: "Romans", Abbrev: "Romans", Testament: NewTestament,
+		ID: Romans, Name: "Romans", Abbrev: "Rom", Testament: NewTestament,
 		Verses: []int{32, 29, 31, 25, 21, 23, 25, 39, 33, 21, 36, 21, 14, 23, 33, 27},
 	},
 	FirstCorinthians: {
-		ID: FirstCorinthians, Name: "FirstCorinthians", Abbrev: "FirstCorinthians", Testament: NewTestament,
+		ID: FirstCorinthians, Name: "1 Corinthians", Abbrev: "1Cor", Testament: NewTestament,
 		Verses: []int{31, 16, 23, 21, 13, 20, 40, 13, 27, 33, 34, 31, 13, 40, 58, 24},
 	},
 	SecondCorinthians: {
-		ID: SecondCorinthians, Name: "SecondCorinthians", Abbrev: "SecondCorinthians", Testament: NewTestament,
+		ID: SecondCorinthians, Name: "2 Corinthians", Abbrev: "2Cor", Testament: NewTestament,
 		Verses: []int{24, 17, 18, 18, 21, 18, 16, 24, 15, 18, 33, 21, 14},
 	},
 	Galatians: {
-		ID: Galatians, Name: "Galatians", Abbrev: "Galatians", Testament: NewTestament,
+		ID: Galatians, Name: "Galatians", Abbrev: "Gal", Testament: NewTestament,
 		Verses: []int{24, 21, 29, 31, 26, 18},
 	},
 	Ephesians: {
-		ID: Ephesians, Name: "Ephesians", Abbrev: "Ephesians", Testament: NewTestament,
+		ID: Ephesians, Name: "Ephesians", Abbrev: "Eph", Testament: NewTestament,
 		Verses: []int{23, 22, 21, 32, 33, 24},
 	},
 	Philippians: {
-		ID: Philippians, Name: "Philippians", Abbrev: "Philippians", Testament: NewTestament,
+		ID: Philippians, Name: "Philippians", Abbrev: "Phil", Testament: NewTestament,
 		Verses: []int{30, 30, 21, 23},
 	},
 	Colossians: {
-		ID: Colossians, Name: "Colossians", Abbrev: "Colossians", Testament: NewTestament,
+		ID: Colossians, Name: "Colossians", Abbrev: "Col", Testament: NewTestament,
 		Verses: []int{29, 23, 25, 18},
 	},
 	FirstThessalonians: {
-		ID: FirstThessalonians, Name: "FirstThessalonians", Abbrev: "FirstThessalonians", Testament: NewTestament,
+		ID: FirstThessalonians, Name: "1 Thessalonians", Abbrev: "1Thess", Testament: NewTestament,
 		Verses: []int{10, 20, 13, 18, 28},
 	},
 	SecondThessalonians: {
-		ID: SecondThessalonians, Name: "SecondThessalonians", Abbrev: "SecondThessalonians", Testament: NewTestament,
+		ID: SecondThessalonians, Name: "2 Thessalonians", Abbrev: "2Thess", Testament: NewTestament,
 		Verses: []int{12, 17, 18},
 	},
 	FirstTimothy: {
-		ID: FirstTimothy, Name: "FirstTimothy", Abbrev: "FirstTimothy", Testament: NewTestament,
+		ID: FirstTimothy, Name: "1 Timothy", Abbrev: "1Tim", Testament: NewTestament,
 		Verses: []int{20, 15, 16, 16, 25, 21},
 	},
 	SecondTimothy: {
-		ID: SecondTimothy, Name: "SecondTimothy", Abbrev: "SecondTimothy", Testament: NewTestament,
+		ID: SecondTimothy, Name: "2 Timothy", Abbrev: "2Tim", Testament: NewTestament,
 		Verses: []int{18, 26, 17, 22},
 	},
 	Titus: {
@@ -331,35 +331,35 @@ var books = map[BookID]Book{
 		Verses: []int{16, 15, 15},
 	},
 	Philemon: {
-		ID: Philemon, Name: "Philemon", Abbrev: "Philemon", Testament: NewTestament,
+		ID: Philemon, Name: "Philemon", Abbrev: "Phlm", Testament: NewTestament,
 		Verses: []int{25},
 	},
 	Hebrews: {
-		ID: Hebrews, Name: "Hebrews", Abbrev: "Hebrews", Testament: NewTestament,
+		ID: Hebrews, Name: "Hebrews", Abbrev: "Heb", Testament: NewTestament,
 		Verses: []int{14, 18, 19, 16, 14, 20, 28, 13, 28, 39, 40, 29, 25},
 	},
 	James: {
-		ID: James, Name: "James", Abbrev: "James", Testament: NewTestament,
+		ID: James, Name: "James", Abbrev: "Jas", Testament: NewTestament,
 		Verses: []int{27, 26, 18, 17, 20},
 	},
 	FirstPeter: {
-		ID: FirstPeter, Name: "FirstPeter", Abbrev: "FirstPeter", Testament: NewTestament,
+		ID: FirstPeter, Name: "1 Peter", Abbrev: "1Pet", Testament: NewTestament,
 		Verses: []int{25, 25, 22, 19, 14},
 	},
 	SecondPeter: {
-		ID: SecondPeter, Name: "SecondPeter", Abbrev: "SecondPeter", Testament: NewTestament,
+		ID: SecondPeter, Name: "2 Peter", Abbrev: "2Pet", Testament: NewTestament,
 		Verses: []int{21, 22, 18},
 	},
 	FirstJohn: {
-		ID: FirstJohn, Name: "FirstJohn", Abbrev: "FirstJohn", Testament: NewTestament,
+		ID: FirstJohn, Name: "1 John", Abbrev: "1John", Testament: NewTestament,
 		Verses: []int{10, 29, 24, 21, 21},
 	},
 	SecondJohn: {
-		ID: SecondJohn, Name: "SecondJohn", Abbrev: "SecondJohn", Testament: NewTestament,
+		ID: SecondJohn, Name: "2 John", Abbrev: "2John", Testament: NewTestament,
 		Verses: []int{13},
 	},
 	ThirdJohn: {
-		ID: ThirdJohn, Name: "ThirdJohn", Abbrev: "ThirdJohn", Testament: NewTestament,
+		ID: ThirdJohn, Name: "3 John", Abbrev: "3John", Testament: NewTestament,
 		Verses: []int{14},
 	},
 	Jude: {
@@ -367,7 +367,7 @@ var books = map[BookID]Book{
 		Verses: []int{25},
 	},
 	Revelation: {
-		ID: Revelation, Name: "Revelation", Abbrev: "Revelation", Testament: NewTestament,
+		ID: Revelation, Name: "Revelation", Abbrev: "Rev", Testament: NewTestament,
 		Verses: []int{20, 29, 22, 11, 14, 17, 17, 13, 21, 11, 19, 17, 18, 20, 8, 21, 18, 24, 21, 15, 27, 21},
 	},
 }
