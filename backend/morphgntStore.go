5df02f3c8e91f9a4f68ca1adfb6a1c047fb9cc7c
Andy Holt <andrew.holt@hotmail.co.uk>
2026-04-26 21:13:36 +0100

    replace positional args in query strings with named args

--- backend/morphgntStore.go
+++ backend/morphgntStore.go
@@ -28,23 +28,38 @@ const versesSelect = `
 	WHERE %s
 	ORDER BY book, chapter, verse, word_index`
 
-func versesFilter(ref Reference) (where string, args []any) {
+func versesFilter(ref Reference) (where string, args pgx.NamedArgs) {
 	switch r := ref.(type) {
 	case VerseReference:
-		return "book = $1 AND chapter = $2 AND verse = $3",
-			[]any{r.Book.String(), r.Chapter, r.Verse}
+		return "book = @book AND chapter = @chapter AND verse = @verse",
+			pgx.NamedArgs{
+				"book":    r.Book.String(),
+				"chapter": r.Chapter,
+				"verse":   r.Verse,
+			}
 
 	case RangeReference:
 		if r.Start.Chapter == r.End.Chapter {
-			return "book = $1 AND chapter = $2 AND verse BETWEEN $3 AND $4",
-				[]any{r.Start.Book.String(), r.Start.Chapter, r.Start.Verse, r.End.Verse}
+			return "book = @book AND chapter = @chapter AND verse BETWEEN @start_verse AND @end_verse",
+				pgx.NamedArgs{
+					"book":        r.Start.Book.String(),
+					"chapter":     r.Start.Chapter,
+					"start_verse": r.Start.Verse,
+					"end_verse":   r.End.Verse,
+				}
 		}
-		return `book = $1 AND (
-				   (chapter = $2 AND verse >= $3)
-				OR (chapter > $2 AND chapter < $4)
-				OR (chapter = $4 AND verse <= $5)
+		return `book = @book AND (
+				   (chapter = @start_chapter AND verse >= @start_verse)
+				OR (chapter > @start_chapter AND chapter < @end_chapter)
+				OR (chapter = @end_chapter AND verse <= @end_verse)
 				)`,
-			[]any{r.Start.Book.String(), r.Start.Chapter, r.Start.Verse, r.End.Chapter, r.End.Verse}
+			pgx.NamedArgs{
+				"book":          r.Start.Book.String(),
+				"start_chapter": r.Start.Chapter,
+				"start_verse":   r.Start.Verse,
+				"end_chapter":   r.End.Chapter,
+				"end_verse":     r.End.Verse,
+			}
 	}
 	panic(fmt.Sprintf("unhandled Reference type: %T", ref))
 }
@@ -52,7 +67,7 @@ func versesFilter(ref Reference) (where string, args []any) {
 func (p *PgStore) FetchVerses(ctx context.Context, ref Reference) (Passage, error) {
 	where, args := versesFilter(ref)
 	query := fmt.Sprintf(versesSelect, where)
-	rows, err := p.db.Query(ctx, query, args...)
+	rows, err := p.db.Query(ctx, query, args)
 	if err != nil {
 		return Passage{}, fmt.Errorf("query verses: %w", err)
 	}
