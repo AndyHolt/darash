196820b9a569a75fa01acee9077ac4b4a40d19df
Andy Holt <andrew.holt@hotmail.co.uk>
2026-07-04 00:21:02 +0100

    fix: remove hello world handler (#133)

--- backend/server.go
+++ backend/server.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -18,12 +16,6 @@ func NewServer(m *MorphgntService, t *TahotService) *Server {
 	}
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if _, err := fmt.Fprint(w, "Hello, from Darash!"); err != nil {
-		log.Printf("write response: %v", err)
-	}
-}
-
 func (s *Server) ListenAndServe(addr string) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /health", func(w http.ResponseWriter, _ *http.Request) {
@@ -32,6 +24,5 @@ func (s *Server) ListenAndServe(addr string) error {
 	mux.HandleFunc("GET /api/count", s.morphgntHandler.Count)
 	mux.HandleFunc("GET /api/morphgnt/passage/{ref}", s.morphgntHandler.FetchVerses)
 	mux.HandleFunc("GET /api/tahot/passage/{ref}", s.tahotHandler.FetchVerses)
-	mux.HandleFunc("/", helloHandler)
 	return http.ListenAndServe(addr, mux)
 }
